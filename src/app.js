6786709d186f06a78d37f6175808a6105660243b
anarg1812 <147712294+anarg1812@users.noreply.github.com>
2024-02-27 22:03:00 +0000

    generador de cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let stick = ["♣", "♠", "♥", "♦"];
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let stickRandom = stick[Math.floor(Math.random() * stick.length)];
+  let numberRandom = number[Math.floor(Math.random() * number.length)];
+  console.log(stickRandom);
+  console.log(numberRandom);
+  document.getElementById("top").innerHTML = stickRandom;
+  document.getElementById("number").innerHTML = numberRandom;
+  document.getElementById("bottom").innerHTML = stickRandom;
+
+  if (stickRandom === "♥" || stickRandom === "♥") {
+    document.getElementById("top").style.color = "red";
+    document.getElementById("number").style.color = "red";
+    document.getElementById("bottom").style.color = "red";
+  } else {
+    document.getElementById("top").style.color = "black";
+    document.getElementById("number").style.color = "black";
+    document.getElementById("bottom").style.color = "black";
+  }
 };
